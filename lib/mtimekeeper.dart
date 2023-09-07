db65c0a4183def620cd0ec1bf528ef7da4ac124b
onyim <onyimacr@gmail.com>
2023-09-06 21:18:15 -0600

    Working Login and SignUp pages

--- lib/mtimekeeper.dart
+++ lib/mtimekeeper.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:m_timekeeper/sign_in_page.dart';
+
 class MTimeKeeper extends StatefulWidget {
   const MTimeKeeper({super.key});
 
@@ -7,13 +10,38 @@ class MTimeKeeper extends StatefulWidget {
 }
 
 class _MTimeKeeperState extends State<MTimeKeeper> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  signOut() async {
+    await auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title:  const Text('Leave Projector'),
+      appBar: AppBar(
+        title: const Text('Leave Projector'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+
+            ),
+            //TODO
+            //tooltip: Text(),
+            onPressed: () {
+              signOut();
+              Navigator.pushAndRemoveUntil(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const SignInPage();
+              }), (r) {
+                return false;
+              });
+            },
+          )
+        ],
       ),
     );
   }
 }
-
